273a471b639e2d4975139200e6a353100ece0ffd
OctopusProteins <elijahheejoon@live.com>
2018-08-11 09:46:58 -0400

    Added support to change command prefixes General code cleanup

--- octoprint_discordremote/shared_vars.py
+++ octoprint_discordremote/shared_vars.py
@@ -1,8 +1,14 @@
 #Used for accessing variables across classes when imports are not viable
 
 base_url = ""
+prefix = ""
 
 
-def init(bu):
+def init_baseurl(bu):
     global base_url
     base_url = bu
+
+
+def init_prefix(p):
+    global prefix
+    prefix = p
